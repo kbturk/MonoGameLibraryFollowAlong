5edb7c30730906cde8fa04aefee0cc5d71562ebd
Katherine Turk <kbturk87@gmail.com>
2026-05-31 21:35:59 -0500

    Started working on ch 11: Input Management

---
+++ Input/KeyboardInfo.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework.Input;
+
+namespace MonogameLibrary.Input;
+
+public class KeyboardInfo
+{
+    ///<summary>get the state of the keyboard input during the previous update cycle.</summary>
+
+}
